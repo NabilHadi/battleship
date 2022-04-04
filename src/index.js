32c1cc4984fd68b3d4daf8892230e563fa22c50d
Nabil Hadi <napilfadl@gmail.com>
2022-04-04 21:38:07 +0300

    added Ship and Gameboard factoy functions and test for them

--- src/index.js
+++ src/index.js
@@ -1,16 +1,98 @@
-const randomObject = {
-  length: 100,
-  name: "random",
-  health: 55.5,
+const Ship = function (coordinatesArr = []) {
+  coordinatesArr = coordinatesArr.map((coords) => {
+    return {
+      ...coords,
+      isHit: false,
+    };
+  });
+
+  return {
+    get length() {
+      return coordinatesArr.length;
+    },
+    hit(coords) {
+      coordinatesArr.find((value) => {
+        return value.x === coords.x && value.y === coords.y;
+      }).isHit = true;
+    },
+    isHitAt(coords) {
+      return coordinatesArr.find((value) => {
+        return value.x === coords.x && value.y === coords.y;
+      }).isHit;
+    },
+    isSunk() {
+      return coordinatesArr.every((value) => {
+        return value.isHit === true;
+      });
+    },
+    getHitCoordinates() {
+      return coordinatesArr.filter((value) => {
+        return value.isHit === true;
+      });
+    },
+    getCoordinates() {
+      return coordinatesArr.map((pos) => {
+        return { x: pos.x, y: pos.y };
+      });
+    },
+  };
 };
 
-const randomObject2 = {
-  ...randomObject,
-  age: 26,
-  length: 200,
+const GameBoard = function (_size = 0) {
+  const boardArray = [];
+  const shipsArray = [];
+
+  for (let x = 0; x < _size; x++) {
+    for (let y = 0; y < _size; y++) {
+      boardArray.push({
+        x,
+        y,
+        hasShip: false,
+      });
+    }
+  }
+
+  const isWihinBoardRange = (coords) => {
+    return coords.every((co) => {
+      return co.x >= 0 && co.x < _size && co.y >= 0 && co.y < _size;
+    });
+  };
+
+  const hasShipAt = (coordinates) => {
+    return shipsArray.some((ship) => {
+      const shipCoords = ship.getCoordinates();
+      return shipCoords.some((shipCo) => {
+        return coordinates.some((co) => {
+          return shipCo.x === co.x || shipCo.y === co.y;
+        });
+      });
+    });
+  };
+
+  return {
+    get size() {
+      return _size;
+    },
+    placeShipAt(...coordinates) {
+      if (isWihinBoardRange(coordinates) && !hasShipAt(coordinates)) {
+        const ship = Ship(coordinates);
+        shipsArray.push(ship);
+        coordinates.forEach((co) => {
+          boardArray.find(
+            (pos) => pos.x === co.x && pos.y === co.y
+          ).hasShip = true;
+        });
+        return true;
+      }
+      return false;
+    },
+    getBoard() {
+      return [...boardArray];
+    },
+    getShips() {
+      return [...shipsArray];
+    },
+  };
 };
 
-console.log(randomObject);
-console.log(randomObject2);
-
-export { randomObject };
+export { Ship, GameBoard };
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,9 +1,138 @@
-import { randomObject } from "../index.js";
+import { Ship, GameBoard } from "../index.js";
 
-test("Random Object is defined", () => {
-  expect(randomObject).toBeDefined();
+describe("Ship Object", () => {
+  test("returns correct length", () => {
+    const ship = Ship([
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+    ]);
+    expect(ship.length).toBe(3);
+    const ship2 = Ship([
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+      { x: 0, y: 3 },
+      { x: 1, y: 0 },
+      { x: 1, y: 1 },
+    ]);
+    expect(ship2.length).toBe(6);
+  });
+
+  test("hit function works correctly", () => {
+    const ship = Ship([
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+    ]);
+    ship.hit({ x: 0, y: 0 });
+    expect(ship.isHitAt({ x: 0, y: 0 })).toBe(true);
+    expect(ship.isHitAt({ x: 0, y: 1 })).toBe(false);
+    expect(ship.isHitAt({ x: 0, y: 2 })).toBe(false);
+  });
+
+  test("isHitAt function works correctly", () => {
+    const ship = Ship([
+      { x: 0, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+    ]);
+    expect(ship.isHitAt({ x: 0, y: 0 })).toBe(false);
+    expect(ship.isHitAt({ x: 0, y: 1 })).toBe(false);
+    ship.hit({ x: 0, y: 2 });
+    expect(ship.isHitAt({ x: 0, y: 2 })).toBe(true);
+  });
+
+  test("isSunk function returns correct value", () => {
+    const ship = Ship([
+      { x: 1, y: 0 },
+      { x: 1, y: 1 },
+      { x: 1, y: 2 },
+    ]);
+    expect(ship.isSunk()).toBe(false);
+    ship.hit({ x: 1, y: 0 });
+    ship.hit({ x: 1, y: 1 });
+    ship.hit({ x: 1, y: 2 });
+    expect(ship.isSunk()).toBe(true);
+  });
+
+  test("returns correct hit coordinates", () => {
+    const ship = Ship([
+      { x: 1, y: 0 },
+      { x: 1, y: 1 },
+      { x: 1, y: 2 },
+    ]);
+    ship.hit({ x: 1, y: 1 });
+    expect(
+      ship.getHitCoordinates().every((value) => {
+        if (value.x !== 1 || value.y !== 1) {
+          return false;
+        }
+        return value.isHit;
+      })
+    ).toBe(true);
+    const ship2 = Ship([
+      { x: 5, y: 0 },
+      { x: 5, y: 1 },
+      { x: 5, y: 2 },
+    ]);
+    expect(ship2.getHitCoordinates().length === 0).toBe(true);
+    ship2.hit({ x: 5, y: 1 });
+    expect(ship2.getHitCoordinates().length === 0).toBe(false);
+    expect(ship2.getHitCoordinates().length === 1).toBe(true);
+    ship2.hit({ x: 5, y: 2 });
+    expect(ship2.getHitCoordinates().length === 2).toBe(true);
+  });
 });
 
-test("Random Object has length property", () => {
-  expect(randomObject.length).toBeDefined();
+describe("Gameboard Object", () => {
+  test("returns correct board size", () => {
+    expect(GameBoard(9).size).toBe(9);
+  });
+
+  test("returns correct board state", () => {
+    const gameboard = GameBoard(2);
+    expect(gameboard.getBoard()).toContainEqual({
+      x: 0,
+      y: 0,
+      hasShip: false,
+    });
+    gameboard.placeShipAt({ x: 1, y: 1 });
+    expect(gameboard.getBoard()).toContainEqual({
+      x: 1,
+      y: 1,
+      hasShip: true,
+    });
+  });
+
+  test("getShips function returns correct values", () => {
+    const gameboard = GameBoard(5);
+    expect(gameboard.getShips().length).toBe(0);
+    gameboard.placeShipAt({ x: 1, y: 1 });
+    const shipsArray = gameboard.getShips();
+    expect(shipsArray.length).toBe(1);
+    expect(shipsArray[0].getCoordinates()).toContainEqual({
+      x: 1,
+      y: 1,
+    });
+  });
+
+  test("placeShipAt function only accepts correct positions", () => {
+    const gameboard = GameBoard(5);
+    expect(gameboard.placeShipAt({ x: 0, y: 0 })).toBe(true);
+    expect(gameboard.placeShipAt({ x: 5, y: 5 })).toBe(false);
+    expect(gameboard.placeShipAt({ x: 3, y: 3 })).toBe(true);
+    expect(gameboard.placeShipAt({ x: -1, y: 0 })).toBe(false);
+    expect(gameboard.placeShipAt({ x: 4, y: -1 })).toBe(false);
+  });
+
+  test("placeShipAt function works correctly", () => {
+    const gameboard = GameBoard(5);
+    gameboard.placeShipAt({ x: 4, y: 3 });
+    expect(gameboard.getShips().length).toBe(1);
+    gameboard.placeShipAt({ x: 4, y: 3 });
+    expect(gameboard.getShips().length).toBe(1);
+    gameboard.placeShipAt({ x: 3, y: 2 });
+    expect(gameboard.getShips().length).toBe(2);
+  });
 });
