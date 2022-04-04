13923714d0b2cd4d6a236eb4f075c4f17e45424f
Nabil Hadi <napilfadl@gmail.com>
2022-04-05 01:29:20 +0300

    added recieveAttack function to Gamebaord

--- src/index.js
+++ src/index.js
@@ -16,9 +16,10 @@ const Ship = function (coordinatesArr = []) {
       }).isHit = true;
     },
     isHitAt(coords) {
-      return coordinatesArr.find((value) => {
+      const hitCoords = coordinatesArr.find((value) => {
         return value.x === coords.x && value.y === coords.y;
-      }).isHit;
+      });
+      return hitCoords && hitCoords.isHit;
     },
     isSunk() {
       return coordinatesArr.every((value) => {
@@ -41,56 +42,118 @@ const Ship = function (coordinatesArr = []) {
 const Gameboard = function (_size = 0) {
   const boardArray = [];
   const shipsArray = [];
+  const missedAttacks = [];
 
   for (let x = 0; x < _size; x++) {
     for (let y = 0; y < _size; y++) {
       boardArray.push({
         x,
         y,
-        hasShip: false,
       });
     }
   }
 
-  const isWihinBoardRange = (coords) => {
-    return coords.every((co) => {
+  const isWithinBoardRange = (coordinatesArray) => {
+    return coordinatesArray.every((co) => {
       return co.x >= 0 && co.x < _size && co.y >= 0 && co.y < _size;
     });
   };
 
-  const hasShipAt = (coordinates) => {
-    return shipsArray.some((ship) => {
+  const getShipAt = (coordinates) => {
+    return shipsArray.find((ship) => {
       const shipCoords = ship.getCoordinates();
       return shipCoords.some((shipCo) => {
-        return coordinates.some((co) => {
-          return shipCo.x === co.x || shipCo.y === co.y;
-        });
+        return shipCo.x === coordinates.x && shipCo.y === coordinates.y;
       });
     });
+  };
+
+  const canPlaceShipAt = (coordinatesArray) => {
+    if (!isWithinBoardRange(coordinatesArray)) {
+      return {
+        canPlaceShip: false,
+        reason: `Coordinates (${coordinatesArray}) are out of board range`,
+      };
+    }
+
+    if (
+      coordinatesArray.every((coords) => {
+        return getShipAt(coords);
+      })
+    ) {
+      return {
+        canPlaceShip: false,
+        reason: `Cannot place ship at coordinates (${coordinatesArray}, another ship is in place)`,
+      };
+    }
+
+    return {
+      canPlaceShip: true,
+    };
+  };
+
+  const canAttackAt = (coordinates) => {
+    if (!isWithinBoardRange([coordinates])) {
+      return {
+        canAttack: false,
+        reason: `Coordinates (${coordinates}) are out of board range`,
+      };
+    }
+
+    const ship = getShipAt(coordinates);
+    if (ship && ship.isHitAt(coordinates)) {
+      return {
+        canAttack: false,
+        reason: `Ship at coordinates(${coordinates}) is already hit`,
+      };
+    }
+
+    return {
+      canAttack: true,
+    };
   };
 
   return {
     get size() {
       return _size;
     },
-    placeShipAt(...coordinates) {
-      if (isWihinBoardRange(coordinates) && !hasShipAt(coordinates)) {
-        const ship = Ship(coordinates);
-        shipsArray.push(ship);
-        coordinates.forEach((co) => {
-          boardArray.find(
-            (pos) => pos.x === co.x && pos.y === co.y
-          ).hasShip = true;
-        });
-        return true;
+    placeShipAt(...coordinatesArray) {
+      const canPlaceShipResponse = canPlaceShipAt(coordinatesArray);
+      if (!canPlaceShipResponse.canPlaceShip) {
+        return canPlaceShipResponse;
       }
-      return false;
+      const ship = Ship(coordinatesArray);
+      shipsArray.push(ship);
+      return canPlaceShipResponse;
     },
     getBoard() {
       return [...boardArray];
     },
     getShips() {
       return [...shipsArray];
+    },
+    receiveAttack(coordinates) {
+      const canAttackResponse = canAttackAt(coordinates);
+      if (!canAttackResponse.canAttack) {
+        return canAttackResponse;
+      }
+
+      const ship = getShipAt(coordinates);
+      if (!ship) {
+        missedAttacks.push(coordinates);
+        return { ...canAttackResponse, isMissedShot: true };
+      }
+
+      ship.hit(coordinates);
+      return {
+        ...canAttackResponse,
+        isMissedShot: false,
+      };
+    },
+    isAllShipsSunk() {
+      return shipsArray.every((ship) => {
+        return ship.isSunk();
+      });
     },
   };
 };
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -92,16 +92,14 @@ describe("Gameboard Object", () => {
 
   test("returns correct board state", () => {
     const gameboard = Gameboard(2);
-    expect(gameboard.getBoard()).toContainEqual({
+    expect(gameboard.getBoard()[0]).toMatchObject({
       x: 0,
       y: 0,
-      hasShip: false,
     });
     gameboard.placeShipAt({ x: 1, y: 1 });
-    expect(gameboard.getBoard()).toContainEqual({
+    expect(gameboard.getBoard()[3]).toMatchObject({
       x: 1,
       y: 1,
-      hasShip: true,
     });
   });
 
@@ -119,11 +117,37 @@ describe("Gameboard Object", () => {
 
   test("placeShipAt function only accepts correct positions", () => {
     const gameboard = Gameboard(5);
-    expect(gameboard.placeShipAt({ x: 0, y: 0 })).toBe(true);
-    expect(gameboard.placeShipAt({ x: 5, y: 5 })).toBe(false);
-    expect(gameboard.placeShipAt({ x: 3, y: 3 })).toBe(true);
-    expect(gameboard.placeShipAt({ x: -1, y: 0 })).toBe(false);
-    expect(gameboard.placeShipAt({ x: 4, y: -1 })).toBe(false);
+    expect(gameboard.placeShipAt({ x: 0, y: 0 })).toMatchObject({
+      canPlaceShip: true,
+    });
+    expect(gameboard.placeShipAt({ x: 5, y: 5 })).toMatchObject({
+      canPlaceShip: false,
+    });
+    expect(gameboard.placeShipAt({ x: 3, y: 3 })).toMatchObject({
+      canPlaceShip: true,
+    });
+    expect(gameboard.placeShipAt({ x: -1, y: 0 })).toMatchObject({
+      canPlaceShip: false,
+    });
+    expect(gameboard.placeShipAt({ x: 4, y: -1 })).toMatchObject({
+      canPlaceShip: false,
+    });
+  });
+
+  test("placeShipAt function returns false if coordinates already have a ship there", () => {
+    const gameboard = Gameboard(5);
+    expect(gameboard.placeShipAt({ x: 0, y: 0 })).toMatchObject({
+      canPlaceShip: true,
+    });
+    expect(gameboard.placeShipAt({ x: 0, y: 0 })).toMatchObject({
+      canPlaceShip: false,
+    });
+    expect(gameboard.placeShipAt({ x: 3, y: 3 })).toMatchObject({
+      canPlaceShip: true,
+    });
+    expect(gameboard.placeShipAt({ x: 3, y: 3 })).toMatchObject({
+      canPlaceShip: false,
+    });
   });
 
   test("placeShipAt function works correctly", () => {
@@ -132,7 +156,78 @@ describe("Gameboard Object", () => {
     expect(gameboard.getShips().length).toBe(1);
     gameboard.placeShipAt({ x: 4, y: 3 });
     expect(gameboard.getShips().length).toBe(1);
-    gameboard.placeShipAt({ x: 3, y: 2 });
+    gameboard.placeShipAt({ x: 3, y: 3 });
     expect(gameboard.getShips().length).toBe(2);
+  });
+
+  test("receiveAttack function works with no ships", () => {
+    const gameboard = Gameboard(5);
+    gameboard.receiveAttack({ x: 1, y: 1 });
+  });
+
+  test("receiveAttack function accepts only valid coordinates", () => {
+    const gameboard = Gameboard(5);
+    gameboard.placeShipAt({ x: 0, y: 0 }, { x: 1, y: 1 });
+    expect(gameboard.receiveAttack({ x: 1, y: 1 })).toMatchObject({
+      canAttack: true,
+    });
+    expect(gameboard.receiveAttack({ x: 5, y: 10 })).toMatchObject({
+      canAttack: false,
+    });
+    expect(gameboard.receiveAttack({ x: 0, y: 0 })).toMatchObject({
+      canAttack: true,
+    });
+  });
+
+  test("receiveAttack function reports correct missed attack", () => {
+    const gameboard = Gameboard(5);
+    gameboard.placeShipAt({ x: 1, y: 1 });
+    expect(gameboard.receiveAttack({ x: 2, y: 1 })).toMatchObject({
+      isMissedShot: true,
+    });
+    expect(gameboard.receiveAttack({ x: 1, y: 2 })).toMatchObject({
+      isMissedShot: true,
+    });
+    expect(gameboard.receiveAttack({ x: 1, y: 1 })).toMatchObject({
+      isMissedShot: false,
+    });
+  });
+
+  test("receiveAttack function reports correct hit attack", () => {
+    const gameboard = Gameboard(5);
+    gameboard.placeShipAt({ x: 1, y: 1 }, { x: 2, y: 1 });
+    expect(gameboard.receiveAttack({ x: 1, y: 1 })).toMatchObject({
+      isMissedShot: false,
+      canAttack: true,
+    });
+    expect(gameboard.receiveAttack({ x: 2, y: 1 })).toMatchObject({
+      isMissedShot: false,
+      canAttack: true,
+    });
+    expect(gameboard.receiveAttack({ x: 2, y: 1 })).toMatchObject({
+      canAttack: false,
+    });
+  });
+
+  test("isAllShipsSunk returns correct value", () => {
+    const gameboard = Gameboard(2);
+    gameboard.placeShipAt({ x: 0, y: 0 });
+    gameboard.placeShipAt({ x: 0, y: 1 });
+    gameboard.placeShipAt({ x: 1, y: 1 });
+    expect(gameboard.isAllShipsSunk()).toBe(false);
+    gameboard.receiveAttack({ x: 0, y: 0 });
+    expect(gameboard.isAllShipsSunk()).toBe(false);
+    gameboard.receiveAttack({ x: 0, y: 1 });
+    expect(gameboard.isAllShipsSunk()).toBe(false);
+    gameboard.receiveAttack({ x: 1, y: 1 });
+    expect(gameboard.isAllShipsSunk()).toBe(true);
+
+    const gameboard2 = Gameboard(2);
+    gameboard2.placeShipAt({ x: 0, y: 0 }, { x: 0, y: 1 });
+    expect(gameboard2.isAllShipsSunk()).toBe(false);
+    gameboard2.receiveAttack({ x: 0, y: 0 });
+    expect(gameboard2.isAllShipsSunk()).toBe(false);
+    gameboard2.receiveAttack({ x: 0, y: 1 });
+    expect(gameboard2.isAllShipsSunk()).toBe(true);
   });
 });
