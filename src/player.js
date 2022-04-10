00d5125dff7968b5e23314222f9e0b8bb3a3b5e2
Nabil Hadi <napilfadl@gmail.com>
2022-04-11 00:18:43 +0300

    made AI player moves smarter

--- src/player.js
+++ src/player.js
@@ -13,16 +13,31 @@ const Player = function ({ name: _name, id: _id } = {}) {
 
 export const AIPlayer = function (name, id, enmeyGamebaord) {
   const playerObj = Player({ name, id });
+  const nextMoves = [];
 
   return {
     ...playerObj,
     play() {
-      let coordinates = getRandomCoordinates(enmeyGamebaord.size);
+      let coordinates = nextMoves.pop();
+      if (!coordinates) {
+        coordinates = getRandomCoordinates(enmeyGamebaord.size);
+      }
       let response = enmeyGamebaord.receiveAttack(coordinates);
       while (!response) {
-        coordinates = getRandomCoordinates(enmeyGamebaord.size);
+        coordinates = nextMoves.pop();
+        if (!coordinates) {
+          coordinates = getRandomCoordinates(enmeyGamebaord.size);
+        }
         response = enmeyGamebaord.receiveAttack(coordinates);
       }
+
+      if (!response.isMissedAttack) {
+        nextMoves.push({ x: coordinates.x + 1, y: coordinates.y });
+        nextMoves.push({ x: coordinates.x - 1, y: coordinates.y });
+        nextMoves.push({ x: coordinates.x, y: coordinates.y + 1 });
+        nextMoves.push({ x: coordinates.x, y: coordinates.y - 1 });
+      }
+
       return {
         coordinates,
         response,
